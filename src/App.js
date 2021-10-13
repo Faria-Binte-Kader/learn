feb131f3046fbfbd970daa0c303744297271f67a
Faria-Binte-Kader <fariabintekader67@gmail.com>
2021-10-13 18:09:00 +0600

    pages added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import React from 'react';
 import Navbar from './Components/Navbar';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Home from './Components/pages/Home';
+import Login from './Components/pages/Login';
+import UniList from './Components/pages/UniList';
+import AboutUs from './Components/pages/AboutUs';
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
       <Navbar/>
       <Switch>
         <Route path='/' exact component={Home}/>
+        <Route path='/unilist' component={UniList}/>
+        <Route path='/aboutus' component={AboutUs}/>
+        <Route path='/login' component={Login}/>
       </Switch>
       </Router>
     </>
---
+++ src/Components/pages/UniList.js
@@ -0,0 +1,7 @@
+import React from "react";
+import '../../App.css';
+
+export default function UniList()
+{
+    return <h1 className='unilist'>UniList</h1>
+}
